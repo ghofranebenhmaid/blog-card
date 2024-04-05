38c4a574b95d80a59188396646e628b6c1b112d1
Ghofrane Ben Hmaid <ghofranebh90@gmail.com>
2024-04-05 10:07:15 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="  bg-slate-100 w-full h-[100vh] flex justify-center items-center ">
+      <div className="prose">
+        <h1>Hello, World!</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
